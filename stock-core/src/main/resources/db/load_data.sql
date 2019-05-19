0893e80c3053499da6d83702f41f22f4e65ec4f1
hlherbert <184624975@qq.com>
2019-05-19 23:39:24 +0800

    db

---
+++ stock-core/src/main/resources/db/load_data.sql
@@ -0,0 +1,42 @@
+/* 
+innodb_flush_log_at_trx_commit = 0
+innodb_flush_method = O_DIRECT
+innodb_buffer_pool_size = 200M
+*/
+
+use stock;
+
+DROP TABLE IF EXISTS `stock_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `stock_data` (
+  `date` datetime DEFAULT NULL,
+  `code` varchar(10) DEFAULT NULL,
+  `openPrice` decimal(10,2) DEFAULT NULL,
+  `closePrice` decimal(10,2) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `growPrice` decimal(10,2) DEFAULT NULL,
+  `growPercent` decimal(10,2) DEFAULT NULL,
+  `lowPrice` decimal(10,2) DEFAULT NULL,
+  `highPrice` decimal(10,2) DEFAULT NULL,
+  `amount` decimal(12,0) DEFAULT NULL,
+  `amountMoney` decimal(16,2) DEFAULT NULL,
+  `exchangePercent` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idxUniq` (`code`,`date`),
+  KEY `idxDate` (`date`)
+) ENGINE=InnoDB AUTO_INCREMENT=9061928 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*
+SELECT * FROM stock_data INTO OUTFILE 'dump/data3.txt'
+     FIELDS TERMINATED BY ',' 
+    LINES TERMINATED BY ';';
+	*/
+
+/* copy file to D:\MySQLData\Data\dump */
+
+
+
+LOAD DATA  INFILE 'dump/data_out.txt' IGNORE INTO TABLE stock_data
+  FIELDS TERMINATED BY ',' 
+  LINES TERMINATED BY ';' (`date`, `code`, openPrice, closePrice, id, growPrice, growPercent, lowPrice, highPrice, amount, amountMoney, exchangePercent);
--- stock-core/src/test/java/com/hl/stock/funny/FunnyTest.java
+++ stock-core/src/test/java/com/hl/stock/funny/FunnyTest.java
@@ -4,6 +4,8 @@ import com.hl.stock.core.common.util.SoundUtils;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.*;
+
 @SpringBootTest
 public class FunnyTest {
 
@@ -13,5 +15,44 @@ public class FunnyTest {
         //Toolkit.getDefaultToolkit().beep(); //耳机发声
         //System.out.println("\u0007"); //蜂鸣器发声
         SoundUtils.beep();
+    }
+
+    // 处理股票导出的数据，替换符号
+    @Test
+    public void splitStockData() {
+        System.out.println("begin");
+        String file = "D:\\MySQLData\\Data\\dump\\data.txt";
+        String distfile = "D:\\MySQLData\\Data\\dump\\data_out.txt";
+        long bytes = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(distfile))) {
+            char[] buf = new char[1024 * 1024];
+            int len = 0;
+            while ((len = br.read(buf)) > 0) {
+                for (int i = 0; i < len; i++) {
+                    if (buf[i] == '(') {
+                        buf[i] = ' ';
+                    } else if (i > 0 && buf[i - 1] == ')' && buf[i] == ',') {
+                        buf[i - 1] = ';';
+                        buf[i] = ' ';
+                    } else if (buf[i] == '\'') {
+                        buf[i] = ' ';
+                    }
+                }
+                bw.write(buf, 0, len);
+                bytes += len;
+                System.out.println(bytes / 1024 / 1024 + " MB");
+            }
+            bw.flush();
+            bw.close();
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("end");
     }
 }
